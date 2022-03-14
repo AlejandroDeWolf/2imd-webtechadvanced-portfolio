b4c283786abb098f0c48167de27e4fdb914db4f3
AlejandroDeWolf <dewolf.alejandro@gmail.com>
2022-03-14 12:12:35 +0100

    fix: don't show priority name in list when entering a todo

--- lab3 - es6/todo-app/classes/Todo.js
+++ lab3 - es6/todo-app/classes/Todo.js
@@ -9,15 +9,15 @@ export default class Todo {
 
     if (this.title.startsWith("low:")) {
       li.classList.add("prior-low");
-      li.innerHTML = this.title.slice(4);
+      this.title = this.title.replace("low:", "");
     }
     else if (this.title.startsWith("high:")) {
       li.classList.add("prior-high");
-      li.innerHTML = this.title.slice(5);
+      this.title = this.title.replace("high:", "");
     }
     else if (this.title.startsWith("medium:")) {
       li.classList.add("prior-medium");
-      li.innerHTML = this.title.slice(7);
+      this.title = this.title.replace("medium:", "");
     }
     else {
       li.classList.add("prior-medium");
